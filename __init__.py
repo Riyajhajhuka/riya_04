37d2a04021c9912cce686223a792dbb1fc6eb227
Riyajhajhuka <135508829+Riyajhajhuka@users.noreply.github.com>
2023-06-17 16:15:45 +0530

    __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from telethon import TelegramClient 
+
+import logging
+
+import time
+
+openai_key = "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"
+
+api_id = "1125689"
+
+api_hash = "4772d1792ed194020a8fb06a91ffb8fa"
+
+bot_token = "6213828909:AAHBoKzlyeVRkHBUbF0MvENQ6FjDQgBwQCA"
